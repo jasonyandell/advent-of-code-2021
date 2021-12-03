7de5e17f0786b3bd0c5e7f1abf05f736dfaa01ea
Jason Yandell <jyandell@cj.com>
2021-12-03 12:48:03 -0600

    Day 3, problem 2

--- src/Day03.test.tsx
+++ src/Day03.test.tsx
@@ -47,17 +47,53 @@ const compute = (input:string[]) : number => {
     return gamma*epsilon;
 }
 
+const computeRatingsStrings = (input:string[]) : [oxygenString:string, co2String:string] => {
+    const filterBy = (input:string[], predicate:((value:number, majority:number)=>string)) : string => {
+        const width = input[0].length;
+        let candidates = input;
+        for(let i = 0; i < width; i++) {
+            const columnCounts = getColumnCounts(candidates);
+            const height = candidates.length;
+            const majority = height / 2 ;
+            const mostCommon = columnCounts.map(count => predicate(count, majority));
+
+            candidates = candidates.filter(candidate => {
+                return (candidate[i] === mostCommon[i]);
+            });
+
+            if (candidates.length == 1) break;
+        }
+        return candidates[0];
+    };
+
+    const filterByMostCommon = (input:string[]) : string => {
+        return filterBy(input, (count, majority) => count>=majority?'1':'0');
+    }
+    const filterByLessCommon = (input:string[]) : string => {
+        return filterBy(input, (count, majority) => count<majority?'1':'0');
+    }
+
+    const oxygenString = filterByMostCommon(input);
+    const co2String = filterByLessCommon(input);
+
+    return [oxygenString, co2String]; 
+}
+
+const computeRatings = (input: string[]): [oxygen:number, co2:number] => {
+    const [oxygenString, co2String] = computeRatingsStrings(input);
+    return [toInt(oxygenString), toInt(co2String)];
+}
 
 test('first column has 7 1 bits', () => {
     const actual = countOnBits(sampleInput, 0);
     expect(actual).toBe(7);
-})
+});
 
 test('gamma counts are as described', () => {
     const [gamma, epsilon] = calculateStrings(sampleInput);
     expect(gamma).toBe('10110');
     expect(epsilon).toBe('01001');
-})
+});
 
 test('binary conversion works appropriately', () => {
     const actual = toInt('10110');
@@ -69,7 +105,21 @@ test('Sample output is 198', () => {
     expect(actual).toBe(198);
 });
 
-test('Actual output is ..', () => {
+test('Problem 1 actual output is ..', () => {
     const actual = compute(actualInput);
     console.log("Day 3, Problem 1", actual);
 });
+
+test('Problem 2 sample is 230', () => {
+    const [oxygen, co2] = computeRatings(sampleInput);
+    expect([oxygen, co2]).toEqual([23, 10]);
+
+    const actual = oxygen*co2;
+    expect(actual).toBe(230);
+});
+
+test('Problem 2 actual is ...', () => {
+    const [oxygen, co2] = computeRatings(actualInput);
+    console.log("Day 3, Problem 2", oxygen*co2);
+});
+
