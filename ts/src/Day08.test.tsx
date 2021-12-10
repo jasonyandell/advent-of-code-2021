4ee7a6f84fcf79782b5fed9dfe3539586e24fa0e
Jason Yandell <jyandell@cj.com>
2021-12-10 15:53:59 -0600

    Day 8, Problem 2

--- ts/src/Day08.test.tsx
+++ ts/src/Day08.test.tsx
@@ -1,17 +1,5 @@
 export {}
-import { actualInput } from "./Day08Input";
-import * as _ from "lodash";
-
-const sampleInput = `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
-edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
-fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
-fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
-aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
-fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
-dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
-bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
-egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
-gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`.toString().split("\n")
+import { sampleInput, actualInput } from "./Day08Input";
 
 const getWordGroups = (input:string[]):string[][][] => {
     const splits = input.map(line => {
@@ -20,6 +8,114 @@ const getWordGroups = (input:string[]):string[][][] => {
     })
     const wordGroups = splits.map(line=>line.map(x=>x.split(' ')))
     return wordGroups
+}
+
+const numbersAsLetters:string[] = [
+    "abcefg",
+    "cf",
+    "acdeg",
+    "acdfg",
+    "bcdf",
+    "abdfg",
+    "abdefg",
+    "acf",
+    "abcdefg",
+    "abcdfg"
+]
+
+const permutations = (arr:any[]) : any[][] => {
+    if (arr.length <= 2) return arr.length === 2 ? [arr, [arr[1], arr[0]]] : arr;
+    return arr.reduce(
+      (acc, item, i) =>
+        acc.concat(
+          permutations([...arr.slice(0, i), ...arr.slice(i + 1)]).map(val => [
+            item,
+            ...val,
+          ])
+        ),
+      []
+    );
+  };
+
+const allMaps:string[] = permutations("abcdefg".split('')).map(stringArray => stringArray.reduce((a:string,b)=>a+b),"");
+
+const alphaSortString = (s:string):string =>([...s.split('')].sort()).reduce((a,b)=>a+b);
+
+function getMap(translationMap:string):any {
+    const aCharCode = 'a'.charCodeAt(0);
+    const map = {};
+
+    for(let i=0; i<7; i++) {
+        const curr = String.fromCharCode(i+aCharCode);
+        map[translationMap[i]] = curr
+    }
+    return map;
+}
+
+function applyMap(map:any, input:string):string {
+    let result = "";
+    for(let i=0; i<input.length; i++) {
+        result += map[input[i]];
+    }
+    return alphaSortString(result);
+}
+
+const getDigit = (translated:string):number => numbersAsLetters.indexOf(translated);
+
+const tryTranslationMap = (inputs:string[], map:any) : boolean => {
+    const digits = inputs.map(entry => {
+        const translated = applyMap(map, entry);
+        const digit=getDigit(translated)
+        return digit;
+    })
+    const success = !digits.includes(-1);
+    return success;
+}
+
+function solve(rawInputs:string[], rawOutputs:string[]):number {
+    // 
+    //  dddd
+    // e    a
+    // e    a
+    //  ffff
+    // g    b
+    // g    b
+    //  cccc    
+
+    // generate map.  the above would be 
+    //          "abcdefg"
+    //          "deafgbc", which means map d:a,e:b,a:c,f:d, etc.
+    // apply map to input:
+    // example: 5 in the sample is cdfbe, sorted is bcdef, apply map above
+    // bcdef
+    // fgabd.
+    // sorted: abdfg
+    // checking numbersAsLetters, that's 5, so this one works.  
+    // 
+    // now check all 10. if they all show up in numbersAsLetters, code is good.  apply it to right hand side 
+
+    const sortedInputs = rawInputs.map(alphaSortString)
+    const goodTranslationMap = allMaps.find(translationMap => {
+        const map = getMap(translationMap);
+        return tryTranslationMap(sortedInputs, map)
+    })
+    const workingMap = getMap(goodTranslationMap) // either one works, or crash
+    const sortedOutputs = rawOutputs.map(alphaSortString);
+    const digits = sortedOutputs.map(output => getDigit(applyMap(workingMap, output)))
+    return Number(digits.reduce((a,b)=>a+b,""));
+}
+
+const solveMany = (inputs:string[]) : number[] => {
+    return inputs.map(entry => {
+        const [inputs,outputs] = entry.split(" | ");
+        return solve(inputs.split(' '), outputs.split(' '));
+    })
+}
+
+const problem2 = (inputs:string[]):number => {
+    const allDigits = solveMany(inputs);
+    const sum = allDigits.reduce((a,b)=>a+b)
+    return sum;
 }
 
 const calculate = (input:string[]) : number => {
@@ -40,3 +136,20 @@ test('problem 1 output is..', () => {
     const output = calculate(actualInput)
     console.log("Day 8, Problem 1", output)
 })
+
+test('problem 2, first sample works', () =>{
+    const rawInputs = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab".split(' ');
+    const rawOutputs = "cdfeb fcadb cdfeb cdbaf".split(" ");
+    expect(solve(rawInputs, rawOutputs)).toBe(5353);
+})
+
+test('problem 2, sample input should be 61229', () => {
+    const sum = problem2(sampleInput);
+    expect(sum).toBe(61229)
+})
+
+test('problem 2, actual output is...', () => {
+    const output = problem2(actualInput);
+    console.log("Day 8, Problem 2", output)
+})
+
