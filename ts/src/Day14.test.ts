4e2c65c6bfdb97923aad4809479c6f5c8a47ffc2
Jason Yandell <jyandell@cj.com>
2021-12-14 15:45:21 -0600

    Day 14, Problem 1

--- ts/src/Day14.test.ts
+++ ts/src/Day14.test.ts
@@ -1,4 +1,4 @@
-import {sampleInput} from './Day14Input'
+import {sampleInput, actualInput} from './Day14Input'
 import '@testing-library/jest-dom'
 import _ from 'lodash'
 
@@ -25,25 +25,62 @@ const getCountsFromTemplate = (template:String):Counts => {
     return new Map<string, number>(pairs.map(pair => [pair,1]))
 }
 
-const applyTemplate = (counts:Counts, rules:Rules) => {
-    // for each count in counts, look for matching rule.  if found, apply, 
-    let inputPairs:string[] = []
-    counts.forEach((count,pair) => inputPairs.push(pair));
+type PairCount = {pair:string, count:number}
 
-    const newPairs = inputPairs.map(pair => {
-        if (rules.has(pair)) {
+const applyTemplate = (counts:Counts, rules:Rules):Counts => {
+    // for each count in counts, look for matching rule.  if found, apply, 
+    let inputPairCounts:PairCount[] = Array.from(counts.entries()).map( ([pair,count]) => ({pair,count}) )
+
+    const newPairs = inputPairCounts.map(pairCount => {
+        if (rules.has(pairCount.pair)) {
+            const {pair, count} = pairCount
+            //newCounts.set(pair, (newCounts.get(pair)??1)-1)
+
             const element = rules.get(pair);
             const [first,second] = pair.split('');
-            return [first+element,element+second]
+            return [ {pair:first+element, count}, {pair:element+second, count}]
         }
         else {
-            return [pair]
+            return [pairCount]
         }
     }).flat()
-    console.log(newPairs)
+
+    const newCounts = new Map<string,number>()
+    newPairs.forEach(({pair,count}) => newCounts.set(pair, (newCounts.get(pair) ?? 0) + count))
+
+    return newCounts;
 }
 
-test('inputs', () => {
-    const inputs = inputFromString(sampleInput);
-    applyTemplate(getCountsFromTemplate(inputs.template), inputs.rules)
-})
+const getMostLeastCommon = (counts:Counts) => {
+    const elements = new Map<string, number>()
+    counts.forEach((v,k) => {
+        //elements.set(k[0], (elements.get(k[0]) ?? 0) + v)
+        elements.set(k[1], (elements.get(k[1]) ?? 0) + v)  // always ENDS, doesn't always start
+    });
+
+    const appearances = Array.from(elements.values())
+    const most = appearances.reduce((most,curr)=>Math.max(most,curr))
+    const least = appearances.reduce((least,curr)=>Math.min(least,curr))
+    return most-least
+}
+
+const run = (input:string, iterations:number):number => {
+    const inputs = inputFromString(input);
+
+    const initialCounts = getCountsFromTemplate(inputs.template)
+    let currCounts = initialCounts;
+    for (let i = 0; i < iterations; i++) {
+        currCounts = applyTemplate(currCounts, inputs.rules)
+    }
+
+    return getMostLeastCommon(currCounts);
+
+}
+
+test('problem 1, sample input', () => {
+    expect(run(sampleInput, 10)).toBe(1588)
+}) 
+
+test('problem 1, actual input', () => {
+    console.log("Day 14, Problem 1", run(actualInput, 10))
+}) 
