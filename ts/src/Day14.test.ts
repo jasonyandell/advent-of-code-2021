078fcfc23c83503ca757c09ea9ec15d3fc88c6cf
Jason Yandell <jyandell@cj.com>
2021-12-14 16:23:44 -0600

    Day 14

--- ts/src/Day14.test.ts
+++ ts/src/Day14.test.ts
@@ -8,12 +8,10 @@ type Counts = Map<string, number>
 
 const inputFromString = (input:string) : Input => {
     const inputs = input.split('\n')
-
     const templates:[string,string][] = inputs.slice(2).map(s=> {
         const [a,b] = s.split(' -> ')
         return [a,b]
     })
-
     return {
         template: inputs[0],
         rules:new Map<string,string>(templates)
@@ -29,13 +27,11 @@ type PairCount = {pair:string, count:number}
 
 const applyTemplate = (counts:Counts, rules:Rules):Counts => {
     // for each count in counts, look for matching rule.  if found, apply, 
-    let inputPairCounts:PairCount[] = Array.from(counts.entries()).map( ([pair,count]) => ({pair,count}) )
+    const inputPairCounts:PairCount[] = Array.from(counts.entries()).map( ([pair,count]) => ({pair,count}) )
 
     const newPairs = inputPairCounts.map(pairCount => {
         if (rules.has(pairCount.pair)) {
             const {pair, count} = pairCount
-            //newCounts.set(pair, (newCounts.get(pair)??1)-1)
-
             const element = rules.get(pair);
             const [first,second] = pair.split('');
             return [ {pair:first+element, count}, {pair:element+second, count}]
@@ -54,8 +50,7 @@ const applyTemplate = (counts:Counts, rules:Rules):Counts => {
 const getMostLeastCommon = (counts:Counts) => {
     const elements = new Map<string, number>()
     counts.forEach((v,k) => {
-        //elements.set(k[0], (elements.get(k[0]) ?? 0) + v)
-        elements.set(k[1], (elements.get(k[1]) ?? 0) + v)  // always ENDS, doesn't always start
+        elements.set(k[1], (elements.get(k[1]) ?? 0) + v)  // character always appears at ENDS, not always at start -- ie last character in string wont appear as start of pair
     });
 
     const appearances = Array.from(elements.values())
@@ -66,15 +61,9 @@ const getMostLeastCommon = (counts:Counts) => {
 
 const run = (input:string, iterations:number):number => {
     const inputs = inputFromString(input);
-
     const initialCounts = getCountsFromTemplate(inputs.template)
-    let currCounts = initialCounts;
-    for (let i = 0; i < iterations; i++) {
-        currCounts = applyTemplate(currCounts, inputs.rules)
-    }
-
+    const currCounts = _.range(0,iterations).reduce( (counts, i) => applyTemplate(counts, inputs.rules), initialCounts)
     return getMostLeastCommon(currCounts);
-
 }
 
 test('problem 1, sample input', () => {
@@ -84,3 +73,12 @@ test('problem 1, sample input', () => {
 test('problem 1, actual input', () => {
     console.log("Day 14, Problem 1", run(actualInput, 10))
 }) 
+
+test('problem 2, sample input', () => {
+    expect(run(sampleInput, 40)).toBe(2188189693529)
+}) 
+
+test('problem 2, actual input', () => {
+    console.log("Day 14, Problem 2", run(actualInput, 40))
+}) 
+
